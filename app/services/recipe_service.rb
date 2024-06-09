6faa95ab1e9ebf071054400f0b1ed3d3e9049cb7
Mel <mel@Mels-MacBook-Pro.local>
2024-06-09 14:32:15 -0600

    Recipe service set up

--- app/services/recipe_service.rb
+++ app/services/recipe_service.rb
@@ -1,32 +1,18 @@
 class RecipeService
 
-  def by_country(country)
-    url = "/api/v1/recipes"
-    params = {
-      type: "public",
-      q: country
-      }
-
-    get_url(url, params)
+  def get_recipes_by_country(country)
+    get_url("api/v1/recipes/type=public&q=#{country}")    
   end
 
   def conn
     Faraday.new(url: 'https://api.edamam.com/') do |faraday|
+      faraday.params[:app_id] = Rails.application.credentials.edamam_id[:key]
+      faraday.params[:app_key] = Rails.application.credentials.edamam[:api_key]
     end
   end
 
   def get_url(url, params = {})
-    response = conn.get(url) do |request|
-      request.params = params
-      request.params["app_id"] = Rails.application.credentials.key[:edamam_id]
-      request.params["app_key"] = Rails.application.credentials.api_key[:edamam]
-    end
-
-    if response.success?
-      JSON.parse(response.body, symbolize_names: true)
-    else
-      # rework this but for now put this basic shit
-      { error: "Failed to fetch data." }
-    end
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
--- spec/services/recipe_service_spec.rb
+++ spec/services/recipe_service_spec.rb
@@ -2,32 +2,29 @@ require 'rails_helper'
 
 RSpec.describe RecipeService do
 
+  it "exists" do
+    service = RecipeService.new
 
+    expect(service).to be_an_instance_of RecipeService
+  end
 
-  ## From Consultancy if needed:
-  # it "exists" do
-  #   service = RecipeService.new
+  it "#conn" do
+    service = RecipeService.new
+    connection = service.conn
 
-  #   expect(service).to be_an_instance_of RecipeService
-  # end
+    expect(connection).to be_an_instance_of Faraday::Connection
+    expect(connection.url_prefix.to_s).to eq("https://api.edamam.com/")
+  end
 
-  # it "#conn" do
-  #   service = RecipeService.new
-  #   connection = service.conn
+  it "#get_url" do
+    # Stub the Faraday connection to return a mock response
+    allow_any_instance_of(Faraday::Connection).to receive(:get)
+    .with('test')
+    .and_return(double(body: '{"message": "sldkjslfls"}'))
 
-  #   expect(connection).to be_an_instance_of Faraday::Connection
-  #   expect(connection.url_prefix.to_s).to eq("http://localhost:5000/")
-  # end
+    service = RecipeService.new
+    response = service.get_url('test')
 
-  # it "#get_url" do
-  #   # Stub the Faraday connection to return a mock response
-  #   allow_any_instance_of(Faraday::Connection).to receive(:get)
-  #   .with('test')
-  #   .and_return(double(body: '{"message": "sldkjslfls"}'))
-
-  #   service = RecipeService.new
-  #   response = service.get_url('test')
-
-  #   expect(response).to eq({ message: 'sldkjslfls' })
-  # end
+    expect(response).to eq({ message: 'sldkjslfls' })
+  end
 end
