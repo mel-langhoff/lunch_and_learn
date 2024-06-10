6419d1015064aa2605f6ef3b704c4ae78114fcf9
Mel <mel@Mels-MacBook-Pro.local>
2024-06-10 16:52:03 -0600

    Endpoint 4 functioning, postman test confirm

--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -1,33 +1,33 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe "Users Api" do
-#   before :each do
-#     host! 'localhost:3000'
+RSpec.describe "Users Api" do
+  before :each do
+    host! 'localhost:3000'
 
-#     @user_params =
-#     {
-#       email: "blah3@jaja.com",
-#       name: "Blahblah",
-#       password: "test",
-#       password_confirmation: "test"
-#     }
-#   end
+    @user_params =
+    {
+      email: "blah@jaja.com",
+      name: "Blah",
+      password: "blah",
+      password_confirmation: "blah"
+    }
+  end
 
-#   describe "/api/v1/users" do
-#     it "can create a user" do
-#       post "/api/v1/users", params: @user_params
+  describe "/api/v1/users" do
+    it "can create a user" do
+      post "/api/v1/users", params: @user_params
 
-#       expect(response).to have_http_status(:created)
+      expect(response).to have_http_status(:created)
 
-#       json_response = JSON.parse(response.body, symbolize_names: true)
-#       expect(json_response[:data][:type]).to eq("user")
-#       expect(json_response[:data][:attributes][:name]).to eq(@user_params[:name])
-#       expect(json_response[:data][:attributes][:email]).to eq(@user_params[:email])
-#       expect(json_response[:data][:attributes][:api_key]).to be_present
+      json_response = JSON.parse(response.body, symbolize_names: true)
+      expect(json_response[:data][:type]).to eq("user")
+      expect(json_response[:data][:attributes][:name]).to eq(@user_params[:name])
+      expect(json_response[:data][:attributes][:email]).to eq(@user_params[:email])
+      expect(json_response[:data][:attributes][:api_key]).to be_present
 
-#       user = User.last
-#       expect(user.name).to eq(@user_params[:name])
-#       expect(user.email).to eq(@user_params[:email])
-#     end
-#   end
-# end
+      user = User.last
+      expect(user.name).to eq(@user_params[:name])
+      expect(user.email).to eq(@user_params[:email])
+    end
+  end
+end
