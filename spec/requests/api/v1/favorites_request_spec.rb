2d06adfd9dd1fdcb227b59986ebee1d9306b0355
Mel <mel@Mels-MacBook-Pro.local>
2024-06-10 20:37:37 -0600

    Fave index request spec complete

--- spec/requests/api/v1/favorites_request_spec.rb
+++ spec/requests/api/v1/favorites_request_spec.rb
@@ -39,8 +39,46 @@ RSpec.describe "Sessions Api" do
   end
 
   it "can show all a user's favorites" do
-    # can be sent as a query param for Endpoint 6 (reqs list)
-    get "/api/v1/favorites", params: { api_key: @user.api_key }
+    user = User.create!(    
+      email: "lksdsljfdsdfsf@jaja.com",
+      name: "Rando User",
+      password: "test",
+      password_confirmation: "test",
+      api_key: SecureRandom.hex(16)
+    )
+    fave1 = user.favorites.create!(
+      country: "Spain",
+      recipe_link: "www.qwertyuiop.com",
+      recipe_title: "Paella"
+    )
 
+    fave2 = user.favorites.create!(
+      country: "Canada",
+      recipe_link: "www.asdfghjkl.com",
+      recipe_title: "Poutine"
+    )
+
+    # can be sent as a query param for Endpoint 6 (reqs list)
+    get "/api/v1/favorites", params: { api_key: user.api_key }
+
+    json_response = JSON.parse(response.body, symbolize_names: true)
+    favorites = json_response.map do |favorite|
+      favorite[:data][:attributes]
+    end
+
+    expect(favorites).to be_an(Array)
+    expect(favorites.size).to eq(2)
+
+    # favorites = [{:country=>"Canada", :recipe_title=>"Poutine", :recipe_link=>"www.asdfghjkl.com", :created_at=>"2024-06-11T02:33:41.001Z"}, {:country=>"Spain", :recipe_title=>"Paella", :recipe_link=>"www.qwertyuiop.com", :created_at=>"2024-06-11T02:33:40.998Z"}]
+    # favorites = [fave2, fave1]
+    # fave2
+    expect(favorites[0][:recipe_title]).to eq("Poutine")
+    expect(favorites[0][:recipe_link]).to eq("www.asdfghjkl.com")
+    expect(favorites[0][:country]).to eq("Canada")
+
+    # fave1
+    expect(favorites[1][:recipe_title]).to eq("Paella")
+    expect(favorites[1][:recipe_link]).to eq("www.qwertyuiop.com")
+    expect(favorites[1][:country]).to eq("Spain")
   end
 end
