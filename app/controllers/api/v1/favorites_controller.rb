6ffe862ed567a210f5f18312de55f8844f58cac4
Mel <mel@Mels-MacBook-Pro.local>
2024-06-10 18:33:07 -0600

    Controller setup complete

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -4,9 +4,21 @@ class Api::V1::FavoritesController < ApplicationController
     user = User.find_by(api_key: params[:api_key])
 
     if user
-      favorite = Favorite.create
+      favorite = user.favorites.new(favorite_params)
+
+      if favorite.save
+        render json: FavoriteSerializer.new(favorite_params), status: :created
+      else
+        render json: { errors: favorite.errors.full_messages.to_sentence }, status: :unprocessable_entity
+      end
     else
-      # error for incorrect API key
+      render json: { error: "Incorrect API key." }, status: :unauthorized
     end
+  end
+
+  private
+
+  def favorite_params
+    params.permit(:country, :recipe_link, :recipe_title)
   end
 end
